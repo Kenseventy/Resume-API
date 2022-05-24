471dacfb8667f982ac1d2e665a4026f7dbbbfcc3
unknown <kmcomptech@gmail.com>
2022-05-23 21:42:35 -0500

    change authentication

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const req = require('express/lib/request');
+const basicAuth = require('express-basic-auth')
 const app = express();
 const fs = require("fs")
 
 const resume = require("./resume.json")
 
 
+app.use(basicAuth({
+    users: { 'admin': 'supersecret',
+            'resume': "resume" }
+}))
 
 
 app.get('/api', (req, res) => {
