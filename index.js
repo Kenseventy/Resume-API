652d0985735b3ddad9736ea0a2a2808224f4d383
Kenseventy <kmcomptech@gmail.com>
2024-03-07 14:05:18 -0600

    Added visitor counter

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const basicAuth = require('express-basic-auth')
 const app = express();
 const fs = require("fs")
 const resume = require("./resume.json")
-
+const axios = require('axios');
 var azure = require('azure-storage');
 
 
@@ -21,12 +21,23 @@ app.use(basicAuth({
     challenge: true
 }))
 
-app.get('/resume', (req, res) => {
-    res.header("Content-Type",'application/json');
-    res.sendFile('resume.json' , {root :__dirname});
-    
-    
-}); 
+
+
+app.get('/resume', async (req, res) => {
+    try {
+        const azureResponse = await axios.get('https://restcounterapikm.azurewebsites.net/api/restcounterapi?code=0RM6aLm518VB3IedjFG9tNb-YpWr9uC6It7M-I-ptoS7AzFuV-hSVQ=='); // Replace 'Azure_Function_URL' with the actual URL
+        const dynamicNumber = azureResponse.data.count; // Assuming the response contains a number property
+
+        let resumeData = JSON.parse(fs.readFileSync(__dirname + '/resume.json', 'utf8'));
+        resumeData.basics.visitCounter = dynamicNumber;
+        
+        res.json(resumeData);
+    } catch (error) {
+        console.error("Failed to fetch the number from Azure Function", error);
+        res.status(500).send("An error occurred");
+    }
+});
+
 
 app.get('/resume/image', (req, res) => {
     res.sendFile(__dirname + '/image.html');
